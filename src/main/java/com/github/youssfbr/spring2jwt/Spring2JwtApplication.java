e26394854350560a3465544bc2fc4493423d8bc8
Alisson Youssf <youssfbr@gmail.com>
2023-10-09 11:52:09 -0300

    Projeto criado

---
+++ src/main/java/com/github/youssfbr/spring2jwt/Spring2JwtApplication.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.spring2jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2JwtApplication.class, args);
+	}
+
+}
